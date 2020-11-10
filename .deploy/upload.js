4d9fceb98e8a2ebf26b297ef1e0445016c7ea106
unnkoel <unnkoel@gmail.com>
2020-11-10 18:34:15 +0800

    add upload

---
+++ .deploy/upload.js
@@ -0,0 +1,22 @@
+const ci = require('miniprogram-ci');
+
+(async () => {
+  const project = new ci.Project({
+    appid: process.env.APP_ID,
+    type: 'miniProgram',
+    projectPath: process.env.PWD,
+    privateKeyPath: `./private.${process.env.APP_ID}.key`,
+    ignores: ['node_modules/**/*'],
+  });
+  const uploadResult = await ci.upload({
+    project,
+    version: process.env.VERSION,
+    desc: `描述：${process.env.DESCRIPTION}`,
+    setting: {
+      es6: true,
+    },
+    robot: 2,
+    onProgressUpdate: console.log,
+  });
+  console.log(uploadResult);
+})();
