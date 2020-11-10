0fac307a86f641aee71bff62da2d26c99887842f
unnkoel <unnkoel@gmail.com>
2020-11-10 14:29:01 +0800

    Add feature for deploying

---
+++ .deploy/preview.js
@@ -0,0 +1,24 @@
+const ci = require('miniprogram-ci');
+const path = require('path');
+
+(async () => {
+  const project = new ci.Project({
+    appid: process.env.APP_ID,
+    type: 'miniProgram',
+    projectPath: path.join(__dirname, '..'),
+    privateKeyPath: path.join(__dirname, `./private.${process.env.APP_ID}.key`),
+    ignores: ['node_modules/**/*'],
+  });
+  const previewResult = await ci.preview({
+    project,
+    desc: '',
+    setting: {
+      es6: true,
+    },
+    robot: 1,
+    qrcodeFormat: 'image',
+    qrcodeOutputDest: `${process.env.PREVIEW_PATH}/preview-dev.png`,
+    onProgressUpdate: console.log,
+  });
+  console.log(previewResult);
+})();
